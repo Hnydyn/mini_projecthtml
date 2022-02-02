5a56b069c7d468d7062d8ce627690c2b66b1568d
Hnydyn <nazargnidin0@gmmil.com>
2022-02-02 11:07:28 +0100

    try 6

---
+++ class_js_work1.js
@@ -0,0 +1,32 @@
+let figures=[11,2,13,4,5,6,7,8,19,10];
+let result=(0+1+2+3+4+5+6+7+8+9);
+console.log(result);
+let book= {
+    name: "Green Book",
+    number_of_pages: 110,
+    genre: "Biopic"
+}
+console.log(book);
+let book2= {
+    name: "Green Book",
+    number_of_pages: 110,
+    genre: "Biopic",
+    author:"Viktor Hugo Green"
+}
+console.log(book2);
+let allbooks=[
+     {name: "Green Book", number_of_page: 110, genre: "Biopic"},
+    {name: "Green Book", number_of_pages: 110, genre: "Biopic", author:"Viktor Hugo Green"}
+]
+console.log(allbooks);
+let height=23;
+let width=10;
+console.log(height*width);
+let heightC=10;
+let dC=4;
+console.log(Math.PI*Math.pow(4/2,2)*10);
+let oneSiden=3;
+let sideM=4;
+let k=Math.sqrt(oneSiden**2+sideM**2);
+console.log(k)
+

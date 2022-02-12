4f3bafe2b499bf442046a94251486a5c810633a4
Hnydyn <nazargnidin0@gmmil.com>
2022-02-12 07:59:22 +0100

    try 7

---
+++ class3_js_file.js
@@ -0,0 +1,47 @@
+// let arrayNumbers = [2,17,13,6,22,31,45,66,100,-18];
+//перебрати його циклом while
+// i = 0;
+// while (i < arrayNumbers.length){
+//     console.log(arrayNumbers[i]);
+//     i++
+// };
+//2. перебрати його циклом for
+// for (let i = 0;i <arrayNumbers.length; i++){
+//     console.log(arrayNumbers[i]);
+// }
+//3. перебрати циклом while та вивести  числа тільки з непарним індексом
+// i=0;
+// while (i < arrayNumbers.length) {
+//     if (i % 2 === 1) {
+//         console.log(arrayNumbers[i])
+//     };
+//     i++
+//     };
+//4.перебрати циклом for та вивести  числа тільки з непарним індексом
+// for ( let i = 0;i < arrayNumbers.length; i++){
+//     if (i%2 === 1){
+//         console.log(arrayNumbers[i]);
+//     }
+// }
+//5.перебрати циклом while та вивести  числа тільки парні  значення
+// i=0;
+// while (i < arrayNumbers.length){
+//     if (i%2 === 0){
+//         console.log(arrayNumbers[i]);
+//     };
+//     i++
+// };
+//6перебрати циклом for та вивести  числа тільки парні  значення
+// for (let i = 0;i < arrayNumbers.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(arrayNumbers[i]);
+//     };
+// }
+//7 замінити кожне число кратне 3 на слово "okten"
+// for (let i = 0;i <arrayNumbers.length; i++){
+//     if (arrayNumbers[i]%3 === 0){
+//         arrayNumbers[i]= 'okten';
+//         console.log(arrayNumbers[i]);
+//     }
+// }
+//8
---
+++ home_js_3.js
@@ -0,0 +1,70 @@
+//1.
+//     let number=[1,2,3,4,5]
+//         console.log(number);
+//     let strings=['top','dope','copy','stop','start']
+//         console.log(strings);
+//     let allTypes=[1,'dip',true,false,'end']
+//         console.log(allTypes);
+//2.
+// for (let i = 0; i < 10; i++){
+//     document.write('<div>someValue here</div>')
+// }
+// for (let i = 0; i < 10; i++){
+//     document.write('<div>&{i}I am goint to oktenSchool!</div>')
+// }
+// let j=0;
+// while(j<20){
+//     document.write(`<div>Something what i wannna do tommorow</div>`)
+// }
+// let l=0;
+// while(l<10){
+//     document.write('<div>&{l}Some tasks for me</div>')
+// }
+//3.Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+// let masiw=[1,2,3,4,4,5,5,53,3,43]
+//     for (i = 0;i < masiw.length; ++i){
+//         console.log(masiw[i])
+//     }
+//------------Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+// let string=['null','stonks','stop','start',true,false,'stip',8,9,10]
+// for (i = 0;i < string.length; ++i){
+//     console.log(string[i])
+// }--- Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+// let different=['stan','fun',true,false,1]
+// for (i = 0;i < different.length; ++i){
+//     console.log(different[i])
+// }----------------- Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+// let allStyle=[1,'tik',true,'sit',false,1203,'run',21,'ten','tool']
+//  for (i = 0;i < allStyle.length; ++i){
+// if (typeof allStyle[i] === 'number'){
+//     console.log(allStyle[i]);
+// }
+//  }-Вивести тільки стрінгу)
+// let allString=[1,2,4,'stone','boomble',true,1,false,'smetana','moloko']
+// for (i = 0;i < allString.length; ++i){
+//     if (typeof allString[i] === 'string'){
+//         console.log(allString[i]);
+//     }
+// }-Вивести тільки булеві
+// let allBoolean=[1,2,3,4,false,'stil','boom',true,'elene',false]
+// for (i = 0;i < allBoolean.length; ++i){
+//     if (typeof allBoolean[i] ==='boolean'){
+//         console.log(allBoolean[i]);
+//     }
+// }Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів.
+//  Вивести в консоль всі його елементи в циклі.
+// let allElements=[];
+//     allElements[0]='you';
+//     allElements[1]=1;
+//     allElements[2]=55;
+//     allElements[3]='me';
+//     allElements[4]=true;
+//     allElements[5]='we';
+//     allElements[6]=false;
+//     allElements[7]=25;
+//     allElements[8]='all';
+//     allElements[9]=true;
+//     for ( let i = 0;i < allElements.length; ++i){
+//         console.log((allElements[i]);
+//     }
+
